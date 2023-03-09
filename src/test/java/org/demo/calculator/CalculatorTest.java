fb0e452c84e92101ec8b905aaff9365b70513dd2
Vito Gasteiz <vito.gasteiz@gmx.de>
2023-03-10 00:04:46 +0100

    [MT-1998] Test a calculator class

---
+++ src/test/java/org/demo/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package org.demo.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    void shoulcAddIntegers() {
+        // given
+        Calculator calc = new Calculator();
+
+        // when
+        int result = calc.add(2, 3);
+
+        // then
+        assertEquals(5, result);
+    }
+
+    @Test
+    void shouldMultiplyIntegers() {
+        // given
+        Calculator calc = new Calculator();
+
+        // when
+        int result = calc.multiply(2, 3);
+
+        // then
+        assertEquals(6, result);
+    }
+}
